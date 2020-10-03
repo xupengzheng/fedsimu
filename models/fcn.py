6dbcdc1b13d6a717e376a72f9b7973a94e381968
unknown <you@example.com>
2020-10-03 15:39:13 +0800

    add codes

---
+++ models/fcn.py
@@ -0,0 +1,7 @@
+from torch.nn import Module
+class Fcn(Module):
+    def __init__(self):
+        pass
+        
+    def forward():
+        pass
---
+++ models/models.py
@@ -0,0 +1,16 @@
+from torch.nn import Module
+class Fcn(Module):
+    def __init__(self):
+        pass
+        
+    def forward():
+        pass
+    
+    
+class Lenet(Module):
+    def __init__(self):
+        pass
+        
+    def forward():
+        pass
+    
